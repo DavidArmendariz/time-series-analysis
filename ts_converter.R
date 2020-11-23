0b15ebcb24fa29e07ef16cc4b0eb8b69bc313097
David Armendariz <darmendariz1998@outlook.com>
2020-11-22 19:26:22 -0500

    Updated repo

--- ts_converter.R
+++
@@ -1,9 +0,0 @@
-require(xts)
-require(zoo)
-require(astsa)
-#set working directory
-setwd("C:/Users/User/OneDrive/Escritorio")
-ts_xts <- as.xts(globtemp)
-#this formats ts_xts, but not the csv output
-indexFormat(ts_xts) <- "%B %Y" 
-write.zoo(ts_xts, file="globtemp.csv",sep=",")
